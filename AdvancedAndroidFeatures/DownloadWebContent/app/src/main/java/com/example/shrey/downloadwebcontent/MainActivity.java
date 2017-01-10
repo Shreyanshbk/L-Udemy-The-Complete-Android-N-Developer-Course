db85f8eb9003dc6f0700eeeda1852f2452470471
LAPTOP-IHE0EL3L\Shrey <kakadiya@usc.edu>
2017-01-09 22:20:38 -0800

    Downloading Contents from Web

---
+++ AdvancedAndroidFeatures/DownloadWebContent/app/src/main/java/com/example/shrey/downloadwebcontent/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.shrey.downloadwebcontent;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection= null;
+
+            try {
+
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while(data != -1){
+                    result += (char) data;
+                    data = reader.read();
+                }
+
+                return result;
+            }
+            catch(Exception e){
+                e.printStackTrace();
+                return "Failed!!";
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        String s = "";
+        try {
+            s = task.execute("https://www.ecowebhosting.co.uk/").get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        Log.i("result : ", s);
+
+    }
+}
